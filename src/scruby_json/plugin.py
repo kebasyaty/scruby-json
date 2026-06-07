a97f92c6beb205a010e92c2e792831e322c2aab2
develgen <develgen.dev@gmail.com>
2026-06-07 19:44:33 +0300

    add as_completed and sorting

--- src/scruby_json/plugin.py
+++ src/scruby_json/plugin.py
@@ -8,8 +8,10 @@ from __future__ import annotations
 __all__ = ("ReturnJson",)
 
 
-import concurrent.futures
+import warnings
 from collections.abc import Callable
+from concurrent.futures import Future, ThreadPoolExecutor, as_completed
+from threading import Event
 from typing import Any, final
 
 import orjson
@@ -35,7 +37,8 @@ class ReturnJson(ScrubyPlugin):
         hash_reduce_left: int,
         db_root: str,
         class_model: Any,
-    ) -> list[str] | None:
+        stop_event: Event,
+    ) -> list[Any] | None:
         """Task for find documents.
 
         This method is for internal use.
@@ -43,6 +46,9 @@ class ReturnJson(ScrubyPlugin):
         Returns:
             List of documents as json strings or None.
         """
+        # Suppress warning - RuntimeWarning: coroutine 'Find._task_find' was never awaited
+        warnings.filterwarnings("ignore", category=RuntimeWarning)
+        # Variable initialization
         branch_number_as_hash: str = f"{branch_number:08x}"[hash_reduce_left:]
         separated_hash: str = "/".join(list(branch_number_as_hash))
         leaf_path = Path(
@@ -53,14 +59,16 @@ class ReturnJson(ScrubyPlugin):
                 "leaf.json",
             ),
         )
-        docs: list[str] = []
+        docs: list[Any] = []
         if await leaf_path.exists():
             data_json: bytes = await leaf_path.read_bytes()
             data: dict[str, str] = orjson.loads(data_json) or {}
             for _, val in data.items():
+                if stop_event.is_set():
+                    return None
                 doc = class_model.model_validate_json(val)
                 if filter_fn(doc):
-                    docs.append(val)
+                    docs.append(doc)
         return docs or None
 
     @final
@@ -88,21 +96,35 @@ class ReturnJson(ScrubyPlugin):
         hash_reduce_left: int = scruby_self._hash_reduce_left
         db_root: str = scruby_self._db_root
         class_model: Any = scruby_self._class_model
+        stop_signal = Event()
+        doc: Any | None = None
         # Run quantum loop
-        with concurrent.futures.ThreadPoolExecutor(scruby_self._max_workers) as executor:
-            for branch_number in branch_numbers:
-                future = executor.submit(
+        with ThreadPoolExecutor(scruby_self._max_workers) as executor:
+            futures: list[Future] = [
+                executor.submit(
                     search_task_fn,
                     branch_number,
                     filter_fn,
                     hash_reduce_left,
                     db_root,
                     class_model,
+                    stop_signal,
                 )
+                for branch_number in branch_numbers
+            ]
+            for future in as_completed(futures):
                 docs = await future.result()
                 if docs is not None:
-                    return docs[0]
-        return None
+                    # Get first document
+                    doc = docs[0]
+                    # Cancel all pending tasks in the queue instantly
+                    executor.shutdown(wait=False, cancel_futures=True)
+                    # Trigger the event to tell running tasks to exit
+                    stop_signal.set()
+                    # Stop loop
+                    break
+        # Return document
+        return doc.model_dump_json()
 
     @final
     async def find_many(
@@ -110,6 +132,8 @@ class ReturnJson(ScrubyPlugin):
         filter_fn: Callable = lambda _: True,
         limit_docs: int = 100,
         page_number: int = 1,
+        sort_fn: Callable = lambda doc: doc.created_at,
+        sort_reverse: bool = True,
     ) -> list[str] | None:
         """Asynchronous method for find many documents matching the filter.
 
@@ -127,8 +151,13 @@ class ReturnJson(ScrubyPlugin):
         Returns:
             List of documents as json strings or None.
         """
-        # The `page_number` parameter must not be less than one
-        assert page_number > 0, "`find_many` => The `page_number` parameter must not be less than one."
+        if __debug__:
+            if limit_docs <= 0:
+                msg = "`find_many` => The `limit_docs` parameter must not be less than one."
+                raise AssertionError(msg)
+            if page_number <= 0:
+                msg = "`find_many` => The `page_number` parameter must not be less than one."
+                raise AssertionError(msg)
         # Get Scruby instance
         scruby_self = self.scruby_self()
         # Variable initialization
@@ -137,30 +166,47 @@ class ReturnJson(ScrubyPlugin):
         hash_reduce_left: int = scruby_self._hash_reduce_left
         db_root: str = scruby_self._db_root
         class_model: Any = scruby_self._class_model
+        stop_signal = Event()
+        stop_outer_loop: bool = False
         counter: int = 0
         number_docs_skippe: int = limit_docs * (page_number - 1) if page_number > 1 else 0
         result: list[str] = []
         # Run quantum loop
-        with concurrent.futures.ThreadPoolExecutor(scruby_self._max_workers) as executor:
-            for branch_number in branch_numbers:
-                if number_docs_skippe == 0 and counter >= limit_docs:
-                    return result[:limit_docs]
-                future = executor.submit(
+        with ThreadPoolExecutor(scruby_self._max_workers) as executor:
+            futures: list[Future] = [
+                executor.submit(
                     search_task_fn,
                     branch_number,
                     filter_fn,
                     hash_reduce_left,
                     db_root,
                     class_model,
+                    stop_signal,
                 )
+                for branch_number in branch_numbers
+            ]
+            for future in as_completed(futures):
                 docs = await future.result()
                 if docs is not None:
                     for doc in docs:
                         if number_docs_skippe == 0:
                             if counter >= limit_docs:
-                                return result[:limit_docs]
+                                # Cancel all pending tasks in the queue instantly
+                                executor.shutdown(wait=False, cancel_futures=True)
+                                # Trigger the event to tell running tasks to exit
+                                stop_signal.set()
+                                # Stop loops
+                                stop_outer_loop = True
+                                break
                             result.append(doc)
                             counter += 1
                         else:
                             number_docs_skippe -= 1
+                if stop_outer_loop:
+                    break
+        # Sorting
+        result.sort(key=sort_fn, reverse=sort_reverse)
+        # Convert to JSON
+        result = [item.model_dump_json() for item in result]
+        # Return a document list
         return result or None
